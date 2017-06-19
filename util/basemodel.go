4b53b3e6ade75ff9df8a2354cc63eaa0cf2d5031
zl <jjjachyty@163.com>
2017-06-19 19:51:30 +0800

    no message

--- util/basemodel.go
+++ util/basemodel.go
@@ -3,19 +3,12 @@ package util
 import (
 	"github.com/go-xorm/xorm"
 	_ "github.com/mattn/go-oci8"
-	"pccqcpa.com.cn/components/zlog"
 )
 
 var Engine *xorm.Engine
 
 func init() {
-	var err error
-	Engine, err = xorm.NewEngine("oci8", "tjrpm10/tjrpm@172.168.171.250/APPDEV")
 
-	Engine.ShowSQL(true)
-	if err != nil {
-		zlog.Error("orm 初始化出错", err)
-	}
 }
 
 // //实体的属性获得列名
